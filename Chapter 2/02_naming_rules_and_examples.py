f8db0527635396da5d3e807d15fce6db32047702
Matiwos Kebede <matiwoskebede01@gmail.com>
2025-06-25 06:27:29 +0300

    this python files is added to to the chapter 2

---
+++ Chapter 2/02_naming_rules_and_examples.py
@@ -0,0 +1,18 @@
+# Naming_rules_and_examples for python variables but this rules also apples for funcations, classes and other identifiers
+# rule 1: variable names must letters, numbers, or underscores and cannot start with a number
+# rule 2: variable names cannot be a reserved keyword e.g if, else, for, print, etc.
+# rule 3: variable naems should be meaningful and descriptive and not too long(optional but recommended)
+# rule 4: variable names should be in snake_case for variables and functions, camelCase for classes and UPPPER_CASE for constants (Optional but recommanded)
+
+# examples of bad variable names
+# 1: name@ = "Alex" # invalid variable name its violate rule 1 this includes special characters
+# 2: 1age = 20 # invalid variable age its violate rule 1 its start witha number
+# 3: for = "loop" # invallid variable name its violate rule 2 its is used reserved python keyword
+# 4: x = 2 # its valid but not descriptive  its violate rules 3
+
+
+#examples of good variables names
+name = "Alex" #valid variable and descriptive
+age = 20 # valild variable and descriptive
+print(name)
+print(age)
---
+++ Chapter 2/02_variables_basics.py
@@ -0,0 +1,8 @@
+# Variable basics in python
+# Variables are used to store data values
+
+# Define name and age variable
+name = "Alex"
+age = 20
+print(name) #output will be: Alex
+print(age) #output will be: 20
