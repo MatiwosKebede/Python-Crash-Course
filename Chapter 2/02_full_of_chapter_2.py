498903fda777dcb284a2e8bf81b35aa65d4110e4
Matiwos Kebede <matiwoskebede01@gmail.com>
2025-07-03 06:42:37 +0300

    exercise 7 of chapter is finished

--- Chapter 2/02_full_of_chapter_2.py
+++ Chapter 2/02_full_of_chapter_2.py
@@ -18,17 +18,25 @@ print(alert) #now it display new value of variable alert called Do it!
 should be simple, such as, "Hello phantom, would you like to learn same Python today?'''
  
 name = "Phantom" #name is defined and assign value to name
-print(f"Hello {Phantom}, would you like to learn same Python today?") # print a message using modern formating which f-string which enable us to embedded varible inside string.
+print(f"Hello {name}, would you like to learn same Python today?") # print a message using modern formating which f-string which enable us to embedded varible inside string.
 
 #2.4 Name Cases
 '''Use a variable to represnt a person's name, and then print that person's name in 
 lowercase, uppercase, and title case.'''
 person_name = "Linus Torvald"
-print(person_name.lower) #display all character in lowercase.
-print(person_name.upper) #display all character in uppercase.
-print(person_name.title) #display all character in title case.
+print(person_name.lower()) #display all character in lowercase.
+print(person_name.upper()) #display all character in uppercase.
+print(person_name.title()) #display all character in title case.
 
 #2.5 Famous Quote
 '''Find a quote from a famous person you admire. Print the quote and the name of its author.
 Your output should look something like the following, including the quotations marks.'''
-print('Karl Marx once said: "I am nothing, but I must be everything.') #to print quote we have to use single qoute inside double qoute or vice verse.
+print('Karl Marx once said: "I am nothing, but I must be everything.') #to print quote we have to use double qoute inside single qoute or vice verse.
+
+#2.6 Famous Quote 2
+'''Repeat Exerise 2.5, but this this time, represent line famouse person's name using a variable called famous_person. 
+Then compose your message and represent it with a new variable called message. 
+Print your message.'''
+famous_person = "Eleanor Roosevelt"
+print(f'{famous_person} once said, The future is belongs to those who believe in the beauty of there dreams.') # use f-string formating and put double quote inside single qoute.
+
