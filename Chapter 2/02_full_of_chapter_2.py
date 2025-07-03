c2170bcfb723bbb6f4b55a4f6e3c022e96657a14
Matiwos Kebede <matiwoskebede01@gmail.com>
2025-07-03 05:33:17 +0300

    2nd exercise of chapter 2 is made

---
+++ Chapter 2/02_full_of_chapter_2.py
@@ -0,0 +1,15 @@
+
+    #Try It YourSelf
+#2.1 Simple message
+'''Assign message to the variable, and then print the message'''
+message = "I am code phantom" #define and assign message variable to I am code phantom value.
+print(message) #display the message in consule that assign above.
+
+#2.2 Simple message 
+'''Assign message to the variable, and print the message,
+ then change first variable value to new message then print new value of the message'''
+alert = "Don't do it!" #alert variable assign to value don't do it!
+print(alert) #display alert orginal value.
+alert = "Do it!" #alert variable value is change from Don't do it! to Do it
+print(alert) #now it display new value of variable alert called Do it!
+
