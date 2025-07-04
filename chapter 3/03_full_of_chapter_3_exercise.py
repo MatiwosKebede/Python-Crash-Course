82f8687583cfbc2e9327efd3a20747fcfeb03820
Matiwos Kebede <matiwoskebede01@gmail.com>
2025-07-04 11:33:13 +0300

    chapter full exercise question of chapter 3 is added

---
+++ chapter 3/03_full_of_chapter_3_exercise.py
@@ -0,0 +1,11 @@
+# TRY IT YOURSELF
+
+#exerise 3.1 Names
+'''Store the names of a few of your friends in alist called names.
+Print each person's by accessing each element in the ist, one at a time.'''
+names = ["Alex", "Micheal", "James", "Abel"] #Defining and assigning list.
+print(names[0]) #Output Alex
+print(names[1]) #Output Micheal
+print(names[2]) #Output James
+print(names[3]) #Output Abel
+
